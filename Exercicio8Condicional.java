ec710d5d4cd6f4e26c4c985ae38794d7a573b068
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:45:47 -0300

    Adicionando comentários

--- Exercicio8Condicional.java
+++ Exercicio8Condicional.java
@@ -3,26 +3,27 @@ import java.util.function.DoubleToIntFunction;
 
 public class Exercicio8Condicional {
     public static void main(String[] args) {
-        Scanner le1 = new Scanner(System.in);
-        Scanner le2 = new Scanner(System.in);
-        Scanner le3 = new Scanner(System.in);
-        Scanner le4 = new Scanner(System.in);
-        Scanner le5 = new Scanner(System.in);
-        Scanner le6 = new Scanner(System.in);
+        
+        // Abrindo leitor, que agora se chama "le" pra que possamos ver que ele pode ter o nome que eu escolher
+        Scanner le = new Scanner(System.in);
 
+        // Captando dados de 6 numeros com o usuario e atribuindo cada um a uma variável (de a até f)
         System.out.println("Digite o primeiro numero: ");
-        double a = le1.nextDouble();
+        double a = le.nextDouble();
         System.out.println("Digite o segundo numero: ");
-        double b = le2.nextDouble();
+        double b = le.nextDouble();
         System.out.println("Digite o terceiro numero: ");
-        double c = le3.nextDouble();
+        double c = le.nextDouble();
         System.out.println("Digite o quarto numero: ");
-        double d = le4.nextDouble();
+        double d = le.nextDouble();
         System.out.println("Digite o quinto numero: ");
-        double e = le5.nextDouble();
+        double e = le.nextDouble();
         System.out.println("Digite o sexto numero: ");
-        double f = le6.nextDouble();
+        double f = le.nextDouble();
 
+        /* Abrindo 3 condicionais diferentes para que eu possa avaliar 3 condições que não tem 
+        nada a ver uma com a outra. Se tivessem uma a ver com a outra, faria em um "if" só */
+        
         if (a == b) {
             System.out.println("Os números são iguais!");
         } else {
@@ -43,3 +44,8 @@ public class Exercicio8Condicional {
     }
     
 }
+
+/* Fazer um programa que receba 6 números e exiba se:
+a) A é igual a B
+b) C é diferente de D
+c) E é maior que F */
