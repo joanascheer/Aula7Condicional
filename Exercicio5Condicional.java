16c5fa8588bb6b3e25d33d7a14928cd8b2d2b11e
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:37:39 -0300

    Comentando código

--- Exercicio5Condicional.java
+++ Exercicio5Condicional.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Exercicio5Condicional {
     public static void main(String[] args) {
-        Scanner len1 = new Scanner(System.in);
-        Scanner len2 = new Scanner(System.in);
+        // Abrindo Scanner pois vou coletar dados digitados pelo usuario
+        Scanner leitor = new Scanner(System.in);
 
+        /* Captando valores digitados com a variável criada (n1 e n2), chamando o leitor criado acima */
         System.out.println("Digite o primeiro valor: ");
-        double n1 = len1.nextDouble();
+        double n1 = leitor.nextDouble();
         System.out.println("Digite o segundo valor: ");
-        double n2 = len2.nextDouble();
+        double n2 = leitor.nextDouble();
 
+        /* Abrindo condição: Se os números forem iguais, o programa dá uma mensagem avisando, já que de acordo
+        com o enunciado, os números digitados não podem ser iguais. Se n1 for maior que n2, ele avisa que n1 é maior.
+        Se não (else), ou seja, se n2 for maior que n1, o programa avisa que n2 é maior. */
         if (n1 == n2) {
             System.out.println("Os valores não podem ser iguais.");
         } else if (n1 > n2) {
