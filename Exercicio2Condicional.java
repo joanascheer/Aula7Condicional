cbca616b4caad861c6e6f383f54710ede4240f56
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:50:29 -0300

    Adicionando comentários

--- Exercicio2Condicional.java
+++ Exercicio2Condicional.java
@@ -2,39 +2,6 @@ import java.util.Scanner;
 
 public class Exercicio2Condicional {
     public static void main(String[] args) {
-        Scanner leAltura = new Scanner(System.in);
-        Scanner lePeso = new Scanner(System.in);
-
-        System.out.println("Digite sua altura: ");
-        double altura = leAltura.nextDouble();
-        System.out.println("Digite o seu peso: ");
-        double peso = lePeso.nextDouble();
-
-        double imc = peso / (altura * altura);
-
-        if (imc < 18.5) {
-            System.out.printf("Seu IMC é de %.2f ", imc);
-            System.out.println(". Você está abaixo do peso.");
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            System.out.printf("Seu IMC é de %.2f ", imc);
-            System.out.println(". Seu peso está normal.");
-        } else if (imc >= 25.0 && imc <= 29.9) {
-            System.out.printf("Seu IMC é de %.2f ", imc);
-            System.out.println(". Você está com sobrepeso.");
-        } else if (imc >= 30.0 && imc <= 39.9) {
-            System.out.printf("Seu IMC é de %.2f ", imc);
-            System.out.println(". Você está com obesidade de grau I.");
-        } else {
-            System.out.printf("Seu IMC é de %.2f ", imc);
-            System.out.println(". Você está com obesidade de grau II.");
-        }
-    }
-    
-}
-import java.util.Scanner;
-
-public class Exercicio2Condicional {
-    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
         // Coletando altura e peso do usuário
@@ -68,3 +35,16 @@ public class Exercicio2Condicional {
     }
     
 }
+
+/*
+Crie um algoritmo que receba a altura e peso do usuário, calcule seu
+IMC e exiba sua classificação.
+OBS: Fórmula: IMC = peso/ (altura x altura), a classificação se
+encontra na tabela abaixo:
+
+MENOR QUE 18,5 MAGREZA 
+ENTRE 18,5 E 24,9 NORMAL 
+ENTRE 25,0 E 29,9 SOBREPESO I
+ENTRE 30,0 E 39,9 OBESIDADE II
+MAIOR QUE 40,0 OBESIDADE GRAVE III
+*/
