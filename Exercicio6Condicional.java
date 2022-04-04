f6e94e6a68fe9570b4d9df7bffb832e28db1ed00
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:40:29 -0300

    Comentando código

--- Exercicio6Condicional.java
+++ Exercicio6Condicional.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class Exercicio6Condicional {
     public static void main(String[] args) {
-        Scanner letime1 = new Scanner(System.in);
-        Scanner letime2 = new Scanner(System.in);
-        Scanner legolstime1 = new Scanner(System.in);
-        Scanner legolstime2 = new Scanner(System.in);
+        // Abrindo Scanner pra pegar dados com o usuário
+        Scanner leitor = new Scanner(System.in);
 
+        // Captando dados do nome dos times e seus gols, e armazenando em variáveis
         System.out.println("Qual é o nome do primeiro time? ");
-        String time1 = letime1.nextLine();
+        String time1 = leitor.nextLine();
         System.out.println("Quantos gols o " +time1+ " fez? ");
-        int golsTime1 = legolstime1.nextInt();
+        int golsTime1 = leitor.nextInt();
         System.out.println("Qual é o nome do segundo time? ");
-        String time2 = letime2.nextLine();
+        String time2 = leitor.nextLine();
         System.out.println("Quantos gols o " +time2+ " fez? ");
-        int golsTime2 = legolstime2.nextInt();
+        int golsTime2 = leitor.nextInt();
 
+        /* Fazendo condição. Se os gols do time 1 forem maiores, o programa avisa que o time 1 venceu.
+        Caso contrário, o time 2 venceu. Se não (else), o programa avisa do empate. */
         if (golsTime1 > golsTime2) {
             System.out.println("O vencedor foi o " +time1);
         } else if (golsTime2 > golsTime1) {
