5aaac38ba7d13f582bbfc78e24ad8057e628790d
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:29:09 -0300

    Comentando código

--- Exercicio2Condicional.java
+++ Exercicio2Condicional.java
@@ -31,3 +31,40 @@ public class Exercicio2Condicional {
     }
     
 }
+import java.util.Scanner;
+
+public class Exercicio2Condicional {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        // Coletando altura e peso do usuário
+        System.out.println("Digite sua altura: ");
+        double altura = leitor.nextDouble();
+        System.out.println("Digite o seu peso: ");
+        double peso = leitor.nextDouble();
+
+        // Calculando IMC
+        double imc = peso / (altura * altura);
+
+        /*
+        * Dependendo do resultado do IMC, mostra-se uma mensagem diferente.
+        */
+        if (imc < 18.5) {
+            System.out.printf("Seu IMC é de %.2f ", imc);
+            System.out.println(". Você está abaixo do peso.");
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.printf("Seu IMC é de %.2f ", imc);
+            System.out.println(". Seu peso está normal.");
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            System.out.printf("Seu IMC é de %.2f ", imc);
+            System.out.println(". Você está com sobrepeso.");
+        } else if (imc >= 30.0 && imc <= 39.9) {
+            System.out.printf("Seu IMC é de %.2f ", imc);
+            System.out.println(". Você está com obesidade de grau I.");
+        } else {
+            System.out.printf("Seu IMC é de %.2f ", imc);
+            System.out.println(". Você está com obesidade de grau II.");
+        }
+    }
+    
+}
