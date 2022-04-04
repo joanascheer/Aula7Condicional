e6ebf3fcbafb4739aeccd754509ce41012f565f9
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:48:30 -0300

    Adicionando comentários

--- Exercicio3Condicional.java
+++ Exercicio3Condicional.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 
 public class Exercicio3Condicional {
     public static void main(String[] args) {
+        
+        // Abrindo Scanner para captar dado do usuário
         Scanner leitor = new Scanner(System.in);
 
+        // Armazenando número digitado pelo usuário na variável "número"
         System.out.println("Digite um número");
         int numero = leitor.nextInt();
 
+        // Se o número for maior que 10, o programa avisará. Se não (else), ele também avisa
         if (numero > 10) {
             System.out.println("É MAIOR QUE 10!");
         } else {
