5466fef91097ad1c04d692eccd667f83e4798274
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:51:11 -0300

    Adicionando comentários

--- Exercicio3Condicional.java
+++ Exercicio3Condicional.java
@@ -20,3 +20,8 @@ public class Exercicio3Condicional {
     }
     
 }
+
+/*
+Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido
+for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+*/
