80c1caa0748953cfaee2b6e6635ec15c596c1743
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-04 15:26:15 -0300

    Comentando o código

--- Exercicio1Condicional.java
+++ Exercicio1Condicional.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 
 public class Exercicio1Condicional {
     public static void main(String[] args) {
-        Scanner leNota1 = new Scanner(System.in);
-        Scanner leNota2 = new Scanner(System.in);
-        Scanner leNota3 = new Scanner(System.in);
-        Scanner leNota4 = new Scanner(System.in);
+        // Criando o leitor para captar o número digitado pelo usuário
+        Scanner leNota = new Scanner(System.in);
 
+        // Criando local para digitação das notas
         System.out.println("Digite N1: ");
-        double n1 = leNota1.nextDouble();
+        double n1 = leNota.nextDouble();
         System.out.println("Digite N2: ");
-        double n2 = leNota1.nextDouble();
+        double n2 = leNota.nextDouble();
         System.out.println("Digite N3: ");
-        double n3 = leNota1.nextDouble();
+        double n3 = leNota.nextDouble();
         System.out.println("Digite N4: ");
-        double n4 = leNota1.nextDouble();
+        double n4 = leNota.nextDouble();
 
+        // Calculando a média
         double media = (n1 + n2 + n3 + n4) / 4;
 
+        // Se a média for maior ou igual a 7, o aluno está aprovado. Se não (else), está reprovado.
         if (media >= 7) {
             System.out.printf("O aluno foi aprovado com média %.2f ", media);
+
         } else {
             System.out.printf("O aluno foi reprovado com média %.2f ", media );
         }
     }
-    /*Crie um algoritmo que receba 4 notas de um aluno, calcule a média
-e exiba se o aluno foi aprovado ou não.
-Obs: a média de aprovação é 7*/ 
+    
+    /*  Crie um algoritmo que receba 4 notas de um aluno, calcule a média
+        e exiba se o aluno foi aprovado ou não.
+        Obs: a média de aprovação é 7   */
 }
